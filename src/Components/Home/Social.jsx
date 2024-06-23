b5d2a0f31cf0ffb7be83169fab495546bf953328
bete <beteme390@gmail.com>
2024-06-23 14:04:49 +0300

    header complet

---
+++ src/Components/Home/Social.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Social() {
+  return (
+    <div className="home_social">
+      <a href="" className="home_social-icon" target="blanck">
+        <i className="uil uil-instagram"></i>
+      </a>
+      <a href="" className="home_social-icon" target="blanck">
+      <i className="uil uil-dribbble"></i>
+      </a>
+      <a href="" className="home_social-icon" target="blanck"><i className="uil uil-github-alt"></i></a>
+    </div>
+  );
+}
+
+export default Social;
